baa449816fa23e4b096c70a1b2ce0ab2e8156daf
John Ruddock <john.jxr@pm.me>
2022-05-24 18:56:34 -0400

    Logout enabled on Link click

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,7 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
 const NavBar = () => {
+
+    const history = useHistory()
+
+    const logout = () => {
+        const token = localStorage.getItem('token')
+        axios.post('http://localhost:9000/api/logout', {}, {
+            headers: {
+                authorization: token
+            }
+        })
+        .then(res => {
+          localStorage.removeItem('token')
+          history.push('/login')
+        })
+        .catch(err => console.log(err))
+      }
+
     return (
         <section className="nav-section">
             <div><h2>Client Auth Project</h2></div>
@@ -9,7 +27,7 @@ const NavBar = () => {
                 <Link to="/login">LOGIN</Link>
                 <Link to="/friends">FRIENDLIST</Link>
                 <Link to="/friends/add">ADDFRIEND</Link>
-                <a>LOGOUT</a>
+                <Link onClick={logout}>LOGOUT</Link>
             </nav>
         </section>
     )
